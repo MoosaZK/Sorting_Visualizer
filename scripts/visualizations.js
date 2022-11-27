1698378ff8b9394d04845e29aa9cde4d0a1d3604
MoosaZk <moosazkhan@gmail.com>
2022-11-27 15:03:05 +0500

    updated

--- scripts/visualizations.js
+++ scripts/visualizations.js
@@ -1,12 +1,3 @@
-/*
-    *****************
-    DONE BY:-   TUMMALA KETHAN
-    
-    *****************
-*/
-
-//We only have to change background-color and height of the sorting element.
-
 var speed=1000;
 
 inp_aspeed.addEventListener("input",vis_speed);
@@ -56,10 +47,3 @@ function enable_buttons()
         }
     },c_delay+=delay_time);
 }
-
-/*
-    *****************
-    DONE BY:-   TUMMALA KETHAN
-    
-    *****************
-*/
